48137f6a80e6d9b76f0a00fe9e8c19711ea6f40d
Oth3r <68134921+Oth3r@users.noreply.github.com>
2025-04-20 15:31:02 -0500

    base docs with config.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,20 +6,17 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'DirectionHUD',
+			favicon: '/favicon.png',
+			logo: {
+				src: './src/assets/icon.png',
+			},
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/oth3r/directionhud' }],
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+					label: "Configuration",
+					autogenerate: { directory: 'configuration'}
+				}
 			],
 		}),
 	],
